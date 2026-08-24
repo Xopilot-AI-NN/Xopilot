02504793e6cfc5fa2659325ed5dd536da6f90485
DenBroLiik <denbro228822@gmail.com>
2026-08-24 08:02:52 +0300

    Remove unused files and modules from the project, including: - Deleted stdout file for flycheck0 due to compilation errors related to missing modules (claud_sync, mcp, updates). - Removed GoogleSans-Regular.ttf font file. - Deleted main.py from api_zgft library. - Removed torch_api.mojo and ui_api_ai.mojo files from ui_api_ai library.

--- Services/target/debug/deps/advanced_xopilot-b12f1034bb07539b.d
+++
@@ -1,5 +0,0 @@
-/home/denis/Music/Xopilot/Services/target/debug/deps/advanced_xopilot-b12f1034bb07539b.d: src/lib.rs
-
-/home/denis/Music/Xopilot/Services/target/debug/deps/libadvanced_xopilot-b12f1034bb07539b.rmeta: src/lib.rs
-
-src/lib.rs:
--- Services/target/debug/deps/advanced_xopilot-d9f5ba9c8a24a674.d
+++
@@ -1,5 +0,0 @@
-/home/denis/Music/Xopilot/Services/target/debug/deps/advanced_xopilot-d9f5ba9c8a24a674.d: src/lib.rs
-
-/home/denis/Music/Xopilot/Services/target/debug/deps/libadvanced_xopilot-d9f5ba9c8a24a674.rmeta: src/lib.rs
-
-src/lib.rs:
