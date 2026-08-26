90d213c2c388dc16b67870103d5f26f7f1454160
DenBroLiik <denbro228822@gmail.com>
2026-08-26 18:47:06 +0300

    AES and more info

--- App/settings/encryption_and_security/toggle.py
+++
@@ -1,9 +0,0 @@
-"""
-Файл: /App/settings/encryption_and_security/toggle.py
-Описание: __Совместимый модуль шифрования__.
-           Реализация кнопки находится в папке encryption_and_security/buttons.
-"""
-
-from .buttons.toggle import build_encryption_toggle
-
-__all__ = ["build_encryption_toggle"]
--- App/settings/personalizations/language.py
+++
@@ -1,9 +0,0 @@
-"""
-Файл: /App/settings/personalizations/language.py
-Описание: __Совместимый модуль языка__.
-           Реализация кнопки находится в папке personalizations/buttons.
-"""
-
-from .buttons.language import build_language_select
-
-__all__ = ["build_language_select"]
